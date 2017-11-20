590805d7542b65403a93025528edff4ff4bae4a2
Vadzim-ПК\Vadzim <vadik_karate@mail.ru>
2017-11-20 21:09:57 +0300

    RecyclerView.ItemDecoration + color  red

--- Indoornav/app/src/main/java/by/grsu/ftf/indoornav/adapter/DividerDecoration.java
+++ Indoornav/app/src/main/java/by/grsu/ftf/indoornav/adapter/DividerDecoration.java
@@ -2,6 +2,8 @@ package by.grsu.ftf.indoornav.adapter;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -12,10 +14,12 @@ import android.view.View;
 
 public class DividerDecoration extends RecyclerView.ItemDecoration {
     private final Drawable drawable;
+    private Paint paint;
 
     public DividerDecoration(Context context) {
         int[] atts = {android.R.attr.listDivider};
         drawable = context.obtainStyledAttributes(atts).getDrawable(0);
+        paint = new Paint();
     }
 
     @Override
@@ -29,9 +33,9 @@ public class DividerDecoration extends RecyclerView.ItemDecoration {
             int top = item.getBottom() + ((RecyclerView.LayoutParams) item.getLayoutParams()).bottomMargin;
             int bottom = top + drawable.getIntrinsicHeight();
 
-            drawable.setBounds(left, top, right, bottom);
-            drawable.draw(canvas);
-
+            paint.setStrokeWidth(10);
+            paint.setColor(Color.RED);
+            canvas.drawRect(left, top, right, bottom, paint);
         }
     }
 }
--- Indoornav/app/src/main/java/by/grsu/ftf/indoornav/adapter/RSSIspeedometer.java
+++ Indoornav/app/src/main/java/by/grsu/ftf/indoornav/adapter/RSSIspeedometer.java
@@ -10,10 +10,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import com.example.indoornav.R;
